d9ee00a50a4f3bdc85766697c384d6a4c5c69ffe
amirbh1106 <42180486+amirbh1106@users.noreply.github.com>
2021-09-01 20:45:27 +0300

    finished

--- trafilog/index.js
+++ trafilog/index.js
@@ -1,51 +1,106 @@
 const express = require('express');
-const path = require('path');
 const fs = require('fs');
 const app = express();
-const csvtojsonV2=require("csvtojson");
 const settings = require('./local.config.json')
-const {isMainThread, parentPort } = require('worker_threads');
 const Worker = require('web-worker');
 const csvFilePath='/home/amir/Desktop/trafilog/coordinates_for_node_test.csv'
 const csv=require('csvtojson');
+const converter = require('json-2-csv');
 
 async function setcsv(){
-var data =  await csv().fromFile(csvFilePath);
-
-var result = data.reduce(function(r, o){
+let data =  await csv().fromFile(csvFilePath);
+let result = data.reduce(function(r, o){
   var k = o.vehicle_id; 
-  if (r[k] || (r[k]=[])) r[k].push({key:o.row_id,long: o.longitude, leng: o.latitude});
+  if (r[k] || (r[k]=[])) r[k].push({row_id:o.row_id,vehicle_id:o.vehicle_id,latitude: o.latitude , longitude:o.longitude});
+  // console.log(r)
   return r;
 }, {});
-// console.log(result);
-// startworkers(data[0])
+let count = 0;
+for(var i in result){
+    count++
+}
+let workers = create_workers(settings.numberofserviceworkers);
+if(settings.numberofserviceworkers == 1){
+    useworkers(result, workers , data);
+}else if(count <= settings.numberofserviceworkers){
+  console.log(`no need for more than ${count}` )
+}else{
+    useworkers(result , workers , data);
+}
 }
 setcsv();
+
 
 function create_workers(number){
   var workers = [];
     for(let i=0; i < number; i++){
         const w1 = new Worker("workers file/oneworker.js")
+        w1.postMessage(i)
         workers.push(w1);
     }
-      startworkers("hay" ,workers)
-    // console.log(workers.length)
+    return workers;
 }
-create_workers(settings.numberofserviceworkers);
-function startworkers(info,webworker) {
-  webworker.map((c,i) => {
-   c.postMessage(info);
-    c.addEventListener('message', e => {
-      console.log(e.data);
+
+
+function useworkers(carsinfo,webworkers,rawjson) {
+  var a =[]
+  var workersindexarray = [];
+  for(var i in carsinfo){
+   workersindexarray.push(i)
+   workersindexarray.fill(0)
+  }
+  webworkers.map((c, t) =>{
+    if(a.length < workersindexarray.length){
+       a.push(t)
+    }
+    if(a.length < workersindexarray.length){
+      a.push(t)
+   }
+ })
+
+
+ var carcount = -1
+  for(let car in carsinfo){
+    carcount++;
+    var webworkerindex = a[carcount]
+// console.log(carsinfo)
+   webworkers[webworkerindex].postMessage(carsinfo[car])
+   webworkers[webworkerindex].addEventListener('message', e => {
+    for(let x in e.data){
+        e.data[x].workerid = webworkerindex
+    }
+      for(let d in rawjson){
+        for(let y in e.data){
+          if(rawjson[d].row_id == e.data[y].row_id){
+            rawjson[d].workerid = e.data[y].workerid
+            rawjson[d].getdistance = e.data[y].getdistance
+          }
+        }
+      }
+      // console.log(rawjson)
+      converter.json2csv(rawjson, (err, csv) => {
+        if (err) {
+            throw err;
+        }
+    
+        // print CSV string
+        fs.writeFile('./localcsv', csv, function (err) {
+          if (err) return console.log(err);
+          // console.log('Hello World > helloworld.txt');
+        });
+        // console.log(csv);
     });
-  })
-  
+    });
 }
+
+
+}
+
 // console.log(settings.numberofserviceworkers)
 app.get('/', (request, response) => {
   return response.send('OK');
 });
 
-app.listen(5000, () => {
-  console.log('App is listening on port 5000');
+app.listen(5200, () => {
+  // console.log('App is listening on port 5000');
 });
